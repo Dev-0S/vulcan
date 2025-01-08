264b28f3f91eaa680900bf215b2288423cad53b6
Dev-0S <oly.stanton@hotmail.co.uk>
2025-01-08 22:07:22 +0000

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#000000] to-[#000000e0] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
@@ -8,6 +13,9 @@ export default function HomePage() {
            <span className="text-[hsl(63,91%,64%)] font-mono">Vulcan_</span> CRM
         </h1>
         
+        {posts.map((post) => (
+          <div key={post.id}> {post.name} </div>
+        ))}
       </div>
     </main>
   );
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,3 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { sql } from "drizzle-orm";
 import {
   index,
@@ -10,19 +7,14 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `vulcancrm_${name}`);
 
 export const posts = createTable(
-  "post",
+  "data",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
     name: varchar("name", { length: 256 }),
+    value: varchar("value", { length: 256 }),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
